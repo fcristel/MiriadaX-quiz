4bc47badde865ca93051fde7979dba9a40eaebfc
Cristian Furcila <fcristel@gmail.com>
2015-07-01 23:19:39 +0200

    fin modulo 7
    Añadida una lista de preguntas, con las modificaciones pertinentes, así
    como una función de autoload en caso de buscar una ruta que contiene un
    ID de quiz.

--- routes/index.js
+++ routes/index.js
@@ -9,8 +9,12 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Quiz' });
 });
 
-router.get('/quizes/question', quizController.question);
-router.get('/quizes/answer', quizController.answer);
+//Autoload de comandos con :quizId
+router.param('quizId', quizController.load); //autoload :quizid
+
+router.get('/quizes', quizController.index);
+router.get('/quizes/:quizId(\\d+)', quizController.show);
+router.get('/quizes/:quizId(\\d+)/answer', quizController.answer);
 router.get('/author', authorController.author);
 
 module.exports = router;
