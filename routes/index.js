bb440726cd0b139bb4cb9845f565f196599a9f2c
Cristian Furcila <fcristel@gmail.com>
2015-07-07 15:53:57 +0200

    Moderación de comentarios
    Añadida funcionalidad para que un comentario de un usuario no
    autenticado, no se publique hasta que un usuario logado lo permita y lo
    publique.

--- routes/index.js
+++ routes/index.js
@@ -13,6 +13,7 @@ router.get('/', function(req, res, next) {
 
 //Autoload de comandos con :quizId
 router.param('quizId', quizController.load); //autoload :quizid
+router.param('commentId', commentController.load); //autoload :commentId
 
 //Definición de rutas de sesion
 router.get('/login', sessionController.new);
@@ -32,6 +33,8 @@ router.delete('/quizes/:quizId(\\d+)', sessionController.loginRequired, quizCont
 //Definición de ruta de /quizes para comments
 router.get('/quizes/:quizId(\\d+)/comments/new', commentController.new);
 router.post('/quizes/:quizId(\\d+)/comments', commentController.create);
+router.get('/quizes/:quizId(\\d+)/comments/:commentId(\\d+)/publish',
+                    sessionController.loginRequired, commentController.publish); // deberia ser un PUT
 
 //Definición de ruta de /author
 router.get('/author', authorController.author);
